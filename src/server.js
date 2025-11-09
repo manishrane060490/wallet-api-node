731b6913a022e592164945b0608ab8cb5a592839
Manish Rane <manish..mahadeorane@gmail.com>
2025-11-09 21:02:40 +0530

    cors added

--- src/server.js
+++ src/server.js
@@ -14,6 +14,7 @@ const PORT = process.env.PORT || 5001
 if(process.env.NODE_ENV === "production") job.start();
 
 app.use(express.json())
+app.use(cors({ origin: "*" }));
 app.use(rateLimiter)
 
 app.use("/api/transactions", transactionRoutes)
